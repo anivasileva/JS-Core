c4d75fe340d0ef1f42b7c702156412884fa7fa87
Anelia <anivasileva1995@abv.bg>
2017-05-27 18:34:51 +0300

    Delete ex3DistanceOverTime.js

--- JS Fundamentals/1ex - SYNTAX- OPERATORS AND LOGIC FLOW/ex3DistanceOverTime.js
+++
@@ -1,6 +0,0 @@
-function calc([firstSpeed,secondSpeed,time]){
-    let first=firstSpeed*time/3600*1000;
-    let second=secondSpeed*time/3600*1000;
-    let result=Math.abs(first-second);
-    console.log(result);
-}
