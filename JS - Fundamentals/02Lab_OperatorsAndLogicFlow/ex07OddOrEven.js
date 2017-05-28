99a73cb559fe92bac06a8fd62808903cd63d99d9
Anelia <anivasileva1995@abv.bg>
2017-05-29 00:58:59 +0300

    Add files via upload

---
+++ JS - Fundamentals/02Lab_OperatorsAndLogicFlow/ex07OddOrEven.js
@@ -0,0 +1,9 @@
+function oddOrEven(num) {
+    let rem = num % 2;
+    if (rem == 0)
+        console.log("even");
+    else if (rem == Math.round(rem))
+        console.log("odd");
+    else console.log("invalid")
+
+}
