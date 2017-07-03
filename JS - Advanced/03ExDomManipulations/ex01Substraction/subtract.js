9705d39ea673a22c8255322dee11b8566e7908b7
Anelia <anivasileva1995@abv.bg>
2017-07-03 18:31:44 +0300

    Add files via upload

---
+++ JS - Advanced/03ExDomManipulations/ex01Substraction/subtract.js
@@ -0,0 +1,5 @@
+function subtract() {
+    let num1= Number(document.getElementById('firstNumber').value);
+    let num2=Number(document.getElementById('secondNumber').value);
+    let result = document.getElementById('result').textContent=(num1-num2);
+}
